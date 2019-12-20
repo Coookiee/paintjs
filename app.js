f9325eb84147192025414831e84328242a399c96
Coookiee <sym00243@naver.com>
2019-12-20 19:14:27 +0900

    세번째 커밋

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
-const colors = document.getElementsByClassName("jsColor");
+const canvas = document.getElementById('jsCanvas');     //  canvas부분
+const ctx = canvas.getContext('2d');        //  canvas 부분 가져올때는 getContext('2d') 해야함
+const colors = document.getElementsByClassName('jsColor');      
 const range = document.getElementById('jsRange');
-const mode = document.getElementById("jsMode");
+const mode = document.getElementById('jsMode');
 const saveBtn = document.getElementById('jsSave');
 
-const INITIAL_COLOR = "#2c2c2c";
+const eraseBtn = document.getElementById('jsErase');
+
+//////////////////  고정 전역 변수들     ////////////////
+const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 600;
+const INITIAL_LINE_WITH = 2.5;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-ctx.fillStyle = "white";
+ctx.fillStyle = 'white';
 ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
-ctx.lineWidth = 2.5;
+ctx.lineWidth = INITIAL_LINE_WITH;
 
+/////////////////       가변 전역 변수들     ///////////////////////
 let painting = false;
 let filling = false;
+
+let erase = false;
+let beforeColor = 'white';        //지울때 사용할 색
+
+///////////////////  함수 선언 부분   ////////////////////
+
 
 function stopPainting(){
     painting = false;
 }
-
 function startPainting(){
     painting = true;
+}
+
+
+function stopErasing(){
+    erasing = false;
+}
+
+function startErasing(){
+    erasing = true;
 }
 
 function onMouseMove(event){
@@ -40,32 +59,37 @@ function onMouseMove(event){
         ctx.lineTo(x,y);
         ctx.stroke();
     }
-}
-
+    if(!erase){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
+    else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}   
 function onMouseDown(event){
     painting = true;
     startPainting();
 }
-
 function onMouseUp(event){
     stopPainting();    
 }
-
 function onMouseLeave(event){
     painting = false;
 }
+
+
 
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
 }
-
 function handleRangeChange(event){
     const size = event.target.value;
     ctx.lineWidth = size;
 }
-
 function handleModeClick(){
     if(filling===true){
         filling = false;
@@ -75,19 +99,17 @@ function handleModeClick(){
         filling = true;
         mode.innerText = "Paint";
         ctx.fillStyle=ctx.strokeStyle;
+        beforeColor = ctx.fillStyle;
     }
 }
-
 function handleCanvasClick(){
     if(filling){
         ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
     }
 }
-
 function handleCM(event){
     event.preventDefault();
 }
-
 function handleSaveClick(){
     const image = canvas.toDataURL();
     const link = document.createElement("a");
@@ -95,6 +117,16 @@ function handleSaveClick(){
     link.download = "PaintJS";
     link.click();
 }
+
+//  지우기 버튼을 누르면 현재 배경색을 펜 색깔로 설정해줌
+function handleEraseClick(){
+    console.log(ctx);
+    ctx.strokeStyle = beforeColor;
+}
+
+
+/////////////   동작부분    ///////////////////
+
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
@@ -104,11 +136,12 @@ if(canvas){
     canvas.addEventListener('click',handleCanvasClick);
     canvas.addEventListener("contextmenu",handleCM);
 }
-
-// console.log(Array.from(colors));
-Array.from(colors).forEach(color => 
-    color.addEventListener('click',handleColorClick)
-);
+//  colors 버튼들에 클릭 이벤트 걸어줌
+if(colors){
+    Array.from(colors).forEach(color => 
+        color.addEventListener('click',handleColorClick)
+    );
+}
 
 if(range){
     range.addEventListener('input',handleRangeChange);
@@ -117,6 +150,12 @@ if(range){
 if(mode){
     mode.addEventListener('click',handleModeClick);
 }
+
 if(saveBtn){
     saveBtn.addEventListener("click",handleSaveClick);
 }
+if(eraseBtn){
+    eraseBtn.addEventListener('click',handleEraseClick);
+    eraseBtn.addEventListener('click',handleRangeChange);
+}
+
